6f6af14de4cfccbace6c78380c344195e3f795a1
somyaagarwal20 <140580499+somyaagarwal20@users.noreply.github.com>
2025-05-31 09:17:07 +0530

    Update CORS policy to allow deployed frontend URL

--- EduSyncwebapi/Program.cs
+++ EduSyncwebapi/Program.cs
@@ -40,9 +40,13 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
-              .AllowAnyHeader()
-              .AllowAnyMethod();
+        policy.WithOrigins(
+            "http://localhost:3000",
+            "https://edusync-frontend.azurestaticapps.net"  // Add your deployed frontend URL
+        )
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
     });
 });
 
